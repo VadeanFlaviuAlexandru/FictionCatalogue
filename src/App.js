fd7b81fe0bd5e157ea3607d70fd1d94a877ee5d6
Vadean Flaviu-Alexandru <vadean.flaviu.alex@gmail.com>
2022-09-05 19:28:11 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import SearchIcon from "./search.svg";
+import MovieCard from "./MovieCard";
+
+const API_URL = "http://www.omdbapi.com/?i=tt3896198&apikey=5ee6a240";
+
+const App = () => {
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+    setMovies(data.Search);
+  };
+
+  useEffect(() => {
+    document.getElementById("StartingText").innerHTML="Type in the field above to search for movies, shows or video games 🍿"
+  }, []);
+
+  return (
+    <div className="app">
+      <h1>🛸 Fiction catalogue !!!</h1>
+
+      <div className="search">
+        <input
+          placeholder="Write the title here ✍️"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <img
+          src={SearchIcon}
+          alt="search"
+          onClick={() => searchMovies(searchTerm)}
+        />
+      </div>
+
+      {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => (
+            <MovieCard movie={movie} />
+          ))}
+        </div>
+      ) : (
+        <div className="empty">
+          <h2 id="StartingText"> No movies found 😔 </h2>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default App;
